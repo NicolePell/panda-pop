256f2804fe9bed282a287b0f71c25c66388a6f3f
Nicole <pellicena@gmail.com>
2019-01-30 18:20:31 +0000

    Initial commit from Create React App

---
+++ src/components/PandaPop.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+class PandaPop extends React.Component {
+  render() {
+    return <div>PandaPop</div>
+  }
+}
+
+export default PandaPop
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import PandaPop from './components/PandaPop'
+
+ReactDOM.render(
+  <PandaPop />, 
+  document.querySelector('#root')
+)
