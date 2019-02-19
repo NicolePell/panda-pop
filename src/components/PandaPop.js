e4de61e705425f91363816dab18042dba916d848
Nicole <pellicena@gmail.com>
2019-02-19 14:22:38 +0000

    feat(select video): Pass selected video back to app component from VideoItem

--- src/components/PandaPop.js
+++ src/components/PandaPop.js
@@ -5,7 +5,7 @@ import SearchBar from './SearchBar'
 import VideoList from './VideoList'
 
 class PandaPop extends React.Component {
-  state = { videos: [] }
+  state = { videos: [], selectedVideo: null }
 
   onTermSubmit = async term => {
     const response = await youtube.get('/search', {
@@ -17,12 +17,16 @@ class PandaPop extends React.Component {
     this.setState({ videos: response.data.items })
   }
 
+  onVideoSelect = (video) => {
+    console.log('From the App!', video)
+  }
+
   render() {
     return (
       <div className="ui container">
         <h1>PandaPop</h1>
         <SearchBar onFormSubmit={this.onTermSubmit} />
-        <VideoList videos={this.state.videos} />
+        <VideoList videos={this.state.videos} onVideoSelect={this.onVideoSelect} />
       </div>
     ) 
   }
