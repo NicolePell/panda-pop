d7ff25ae9723315bbb54127190fcd39f04e8f083
Nicole <pellicena@gmail.com>
2019-02-19 14:54:12 +0000

    feat(video player): Embed selected video in an iframe

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -4,7 +4,21 @@ const VideoDetail = ({ video }) => {
   if (!video) {
     return <div>You haven't selected a video yet</div>
   }
-  return <div>{video.snippet.title}</div>
+
+  const videoUrl = `https://www.youtube.com/embed/${video.id.videoId}`
+
+  return (
+    <div>
+      <div className="ui embed">
+        <iframe src={videoUrl} />
+      </div>
+      <div className="ui segment">
+        <h4 className="ui header">{video.snippet.title}</h4>
+        <p>Provided from Youtube channel: {video.snippet.channelTitle}</p>
+        <p>{video.snippet.description}</p>
+      </div>
+    </div>
+  )
 }
 
 export default VideoDetail
