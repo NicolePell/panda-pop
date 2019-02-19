0fa45f2e50335f2454833339e58be15f1fc101f9
Nicole <pellicena@gmail.com>
2019-02-19 15:01:04 +0000

    refactor(styling): Move the video list next to the video player

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -4,7 +4,7 @@ export default axios.create({
   baseURL: 'https://www.googleapis.com/youtube/v3',
   params: {
     part: 'snippet',
-    maxResults: 5,
+    maxResults: 4,
     key: process.env.REACT_APP_PANDAPOP_YOUTUBE_KEY
   }
 })
--- src/components/PandaPop.js
+++ src/components/PandaPop.js
@@ -27,8 +27,16 @@ class PandaPop extends React.Component {
       <div className="ui container">
         <h1>PandaPop</h1>
         <SearchBar onFormSubmit={this.onTermSubmit} />
-        <VideoDetail video={this.state.selectedVideo} />
-        <VideoList videos={this.state.videos} onVideoSelect={this.onVideoSelect} />
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="eleven wide column">
+              <VideoDetail video={this.state.selectedVideo} />
+            </div>
+            <div className="five wide column">
+              <VideoList videos={this.state.videos} onVideoSelect={this.onVideoSelect} />
+            </div>
+          </div>
+        </div>
       </div>
     ) 
   }
